6553cfbf7c6844dfb7e6a25f779df99917e5fe17
Noah Kittleson <noahkittleson@gmail.com>
2016-05-17 16:48:37 -0700

    testing out stuff with pulling text off page in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,9 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class App {
@@ -12,6 +15,17 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/lap", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      //pull time off timer in page
+      //
+      WebDriver wd = new WebDriver();
+      String time = wd.findElement(By.cssSelector("div[id^='main'] span.test")).getText();
 
       model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
