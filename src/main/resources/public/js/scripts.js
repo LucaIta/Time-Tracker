68b6229e0d3c689542cb796a2827a494a91a5bb3
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-05-19 13:22:19 -0700

    took out scripts.js that was preventing form submission

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  // alert("Success!");
-
-  $("form").submit(function(event) {
-    event.preventDefault();
-    //nothing yet
-  });
-
-  $("#test").click(function() {
-    alert("Test Success");
-    location.reload();
-  });
-});
+// $(document).ready(function() {
+//   // alert("Success!");
+//
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     //nothing yet
+//   });
+//
+//   $("#test").click(function() {
+//     alert("Test Success");
+//     location.reload();
+//   });
+// });
